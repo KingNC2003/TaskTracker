fd853afd8a078f0a1ccdae80c6d67112e5baa2d3
Nicholas Cocciemiglio <nicholascoccie@yahoo.com>
2026-08-13 00:48:32 -0500

    Project setup

--- src/main/java/com/project/tasktracker/TasktrackerApplication.java
+++ src/main/java/com/project/tasktracker/TasktrackerApplication.java
@@ -1,4 +1,4 @@
-package com.example.tasktracker;
+package com.project.tasktracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/example/tasktracker/TasktrackerApplicationTests.java
+++ src/test/java/com/example/tasktracker/TasktrackerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.tasktracker;
+package com.project.tasktracker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
